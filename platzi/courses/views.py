c6c9c6b5b11922d81dbc2e6b9acf4b137bae6275
Enguenye <jd.florezg1@uniandes.edu.co>
2019-04-26 09:06:11 -0500

    ip configurada

--- platzi/courses/views.py
+++ platzi/courses/views.py
@@ -7,19 +7,19 @@ import base64
 
 
 def get_data(request):
-    url = 'http://127.0.0.1:8000/courses/llave'
+    url = 'http://3.91.236.55:8000/courses/llave'
 
     page = urlopen(url)
-    llave1 = str('123456789')
+    llave1 = str('AVWLyoRfTAfBACuJ0YZ')
     b = bytearray()
     b.extend(map(ord, llave1))
 
-    url2 = 'http://127.0.0.1:8000/courses/solicitarReporte'
+    url2 = 'http://3.91.236.55:8000/courses/solicitarReporte'
 
     page2 = urlopen(url2)
     documento = str(page2.read()).split("'")[1]
 
-    url3 = 'http://127.0.0.1:8000/courses/solicitarReporteHash'
+    url3 = 'http://3.91.236.55:8000/courses/solicitarReporteHash'
     page3 = urlopen(url3)
     hash1 = page3.read()
     h = hmac.new(b, str(documento).encode(), hashlib.sha256)
